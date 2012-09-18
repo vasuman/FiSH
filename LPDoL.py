179c2b1a0f55dfbb299d3f87bedf0626ce586456
Vasuman Ravichandran <vasumanar@gmail.com>
2012-09-18 19:39:37 +0530

    Added a MulticastSocket object that handles multicast messages

--- LPDoL.py
+++ LPDoL.py
@@ -4,142 +4,142 @@ import pickle
 import socket as sck
 import os
 
-class Peer(dict):
-    def __init__(self, uid, ip_addr, p_index):
-	self['uid']=uid
-	self['ip']=ip_addr
-	self['index']=p_index
+class Peer():
+	'''A class that encapsulates all attributes of a fish node'''
+	def __init__(self, uid, ip_addr, p_index):
+		self.uid=uid
+		self.ip=ip_addr
+		self.index=p_index
 
-    def __str__(self):
-	return '{0[index]}: {0[ip]} - {0[uid]}'.format(self)
+	def __str__(self):
+		return '{0.index}: {0.ip} - {0.uid}'.format(self)
 
 class PeerDiscoveryError(Exception):
-    def __init__(self,code,msg):
-	self.err=code
-	self.msg=msg
+	'''Implements better error handling'''
+	def __init__(self,code,msg):
+		self.err=code
+		self.msg=msg
 
-    def __str__(self):
-	return '{0}: {1}'.format(self.err,self.msg)
+	def __str__(self):
+		return '{0}: {1}'.format(self.err,self.msg)
 
-class Beacon():
+class MulticastSocket(sck.socket):
+	'''An implementation of a multicast socket built on a standard UDP socket specifically for use in LPDoL'''
+	def __init__(self,mcast):
+		super(MulticastSocket,self).__init__(sck.AF_INET,sck.SOCK_DGRAM,sck.IPPROTO_UDP)
+		(self.mcast_addr,self.mcast_port)=mcast
+		self.setsockopt(sck.SOL_SOCKET, sck.SO_REUSEADDR, 1)
+		self.setsockopt(sck.IPPROTO_IP, sck.IP_MULTICAST_TTL, 32)
+		self.bind(('',self.mcast_port))
+		mem = struct.pack('4sl', sck.inet_aton(self.mcast_addr), sck.INADDR_ANY)
+		self.setsockopt(sck.IPPROTO_IP, sck.IP_ADD_MEMBERSHIP, mem)
+		self.settimeout(5)
 	
-	def __init__(self, uid, max_attempt, mcast, tcp_port):
-		n=0
-		(mcast_addr,mcast_port)=mcast
-		self.tcp_port=tcp_port
-		self.mcast_addr=mcast_addr
-		self.mcast_port=mcast_port
+	def sendm(self,msg):
+		return self.sendto(msg,(self.mcast_addr, self.mcast_port))
+	
+class Beacon():
+	'''Broadcasts presence and listens for peer list'''
+	def __init__(self, uid, mcast):
 		self.uid=uid
+		try:
+			self.mc_sock=MulticastSocket(mcast)
+		except sck.error as e:
+			if e.errno is 98:
+				raise PeerDiscoveryError(2,"Another process is using broadcast port")
+			elif e.errno is 19:
+				raise PeerDiscoveryError(5,'No connected interface')
+			else:
+				raise e
+		
+			
+	def udp_broadcast(self):
+		msg='FISH_HOOK:{0}'.format(self.uid)
+		try:
+			self.mc_sock.sendm(msg)
+		except sck.error as e:
+			if x.errno is 101:
+				raise PeerDiscoveryError(1,"Not connected to any network")
+			else:
+				raise e
+
+	def get_peer_list(self):
+		e=PeerDiscoveryError(3,'No valid response')
+		try:
+			msg,addr=self.mc_sock.recvfrom(1024)
+		except sck.timeout:
+			raise e
+		if msg.startswith('FISHEIS:'):
+			self.peer_list=pickle.loads(msg[7:])
+			self.addr_recv=addr
+		else:
+			raise e
+	
+	def burn(self, max_attempt):
+		n=0
 		while True:
 			self.udp_broadcast()
+			if n>max_attempt:
+				self.mc_sock.close()
+				raise PeerDiscoveryError(4,'Maximum tries excceded')
 			try:
-				ip_list=self.get_peer_list(5)
-				self.addr_list=ip_list
+				self.get_peer_list()
+				self.mc_sock.close()
 				break
 			except PeerDiscoveryError as e:
-				if e.err is 2:
+				if e.err is 3:
+					n+=1
 					continue
 				else:
+					self.mc_sock.close()
 					raise e
-			if n>max_attempt:
-				raise PeerDiscoveryError(4,'Maximum tries excceded')
-			n+=1
-
-	def udp_broadcast(self):
-	    udp_sock=sck.socket(sck.AF_INET, sck.SOCK_DGRAM,sck.IPPROTO_UDP)
-	    udp_sock.setsockopt(sck.IPPROTO_IP, sck.IP_MULTICAST_TTL, 32)
-	    try:
-	    	udp_sock.bind(('',self.mcast_port))
-	    except sck.error as e:
-	    	if x.errno is 98:
-			raise PeerDiscoveryError(2,"Another process is using broadcast port")
-		else:
-			print e
-	    msg='FISH_HOOK:{0}'.format(self.uid)
-	    try:
-	    	udp_sock.sendto(msg,(self.mcast_addr, self.mcast_port))
-	    except sck.error as e:
-	    	if x.errno is 101:
-			raise PeerDiscoveryError(1,"Not connected to any network")
-		else:
-			print e
-	    udp_sock.close()
-
-	def get_peer_list(self, timeout):
-	    tcp_sock=sck.socket(sck.AF_INET, sck.SOCK_STREAM)
-	    tcp_sock.bind(('',self.tcp_port))
-	    tcp_sock.listen(1)
-	    tcp_sock.settimeout(timeout)
-	    try:
-		    con_sock,addr=tcp_sock.accept()
-	    except sck.timeout:
-	            tcp_sock.close()
-		    raise PeerDiscoveryError(3,'Wait timed out')
-	    ip_list=con_sock.recv(65536)
-	    con_sock.close()
-	    tcp_sock.close()
-	    return ip_list
-
 
 class Inducter(threading.Thread):
-    def __init__(self, uid, mcast, addr_list):
-    	super(Inducter,self).__init__()
-	self._stop=threading.Event()
-	self.addr_list=addr_list
-	self.uid=uid
-	(self.mcast_addr,self.mcast_port)=mcast
-	self.udp_sock = sck.socket(sck.AF_INET, sck.SOCK_DGRAM, sck.IPPROTO_UDP)
-	self.udp_sock.setsockopt(sck.SOL_SOCKET, sck.SO_REUSEADDR, 1)
-	try:
-		self.udp_sock.bind(('', self.mcast_port))
-	except Exception as e:
-		print e
-	mem = struct.pack('4sl', sck.inet_aton(self.mcast_addr), sck.INADDR_ANY)
-	self.udp_sock.setsockopt(sck.IPPROTO_IP, sck.IP_ADD_MEMBERSHIP, mem)
-	self.udp_sock.settimeout(5)
-	self.tcp_sock=sck.socket(sck.AF_INET, sck.SOCK_STREAM)
-
-    def pI(self):
-    	return True
-    	extract_index=lambda x:self.addr_list[x]['index']
-    	max_uid=max(self.addr_list,key=extract_index)
-	return (max_uid==self.uid)
-
-    def stop(self):
-    	self._stop.set()
-
-    def stopped(self):
-    	return self._stop.isSet()
-
-    def remove(self, e_uid):
-    	del self.addr_list[e_uid]
-
-    def induce(self, e_uid, addr):
-    	last_index=self.addr_list.popitem()[1][2]
-    	new_peer_obj=Peer(e_uid,addr[0],last_index+1)
-	self.addr_list[uid]=new_peer_obj
-	if self.pI():
+	def __init__(self, uid, mcast, addr_list):
+		super(Inducter,self).__init__()
+		self._stop=threading.Event()
+		self.addr_list=addr_list
+		self.uid=uid
 		try:
-			self.tcp_sock.connect((addr[0],self.tcp_port))
-		except Exception as e:
-			print e
-		data_string=pickle.dumps(self.addr_list)
-		self.tcp_sock.sendall(data_string)
-    
-    def run(self):
-	extr_uid=lambda x:x[x.index(':')+1:]
-	while True:
-		if self.stopped():
-			msg='FISH_UNHOOK:{0}'.format(self.uid)
-			self.udp_sock.sendto(msg,(self.mcast_addr,self.mcast_port))
-			self.udp_sock.close()
-			self.tcp_sock.close()
-			break
-		try:
-			msg,addr=self.udp_sock.recvfrom(1024)
-		except sck.timeout:
-			continue	
-		if msg.startswith('FISH_HOOK:'):
-			induce(extr_uid(msg),addr)
-		elif msg.startswith('FISH_UNHOOK:'):
-			remove(extr_uid(msg))
+			self.mc_sock=MulticastSocket(mcast)
+		except sck.error as e:
+			if e.errno is 98:
+				raise PeerDiscoveryError(2,"Another process is using broadcast port")
+			else:
+				raise e
+
+	def stop(self):
+		self._stop.set()
+
+	def stopped(self):
+		return self._stop.isSet()
+
+	def remove_peer(self, e_uid):
+		del self.addr_list[e_uid]
+
+	def add_peer(self, e_uid, addr):
+		last_index=self.addr_list[-1].index
+		new_peer_obj=Peer(e_uid,addr[0],last_index+1)
+		self.addr_list[e_uid]=new_peer_obj
+	
+	def extr_header(self,msg):
+		return msg[msg.index(':')+1:]
+
+	def run(self):
+		while True:
+			if self.stopped():
+				msg='FISH_UNHOOK:{0}'.format(self.uid)
+				self.mc_sock.sendm(msg)
+				break
+			try:
+				msg,addr=self.udp_sock.recvfrom(1024)
+			except sck.timeout:
+				continue
+			if msg.startswith('FISH_HOOK:'):
+				self.add_peer(extr_header(msg),addr)
+				if self.addr_list[-2].uid==self.uid:
+					data_string=pickle.dumps(self.addr_list)
+					self.mc_sock.sendm('FISHIES:{0}'.format(data_string))
+			elif msg.startswith('FISH_UNHOOK:'):
+				self.remove_peer(extr_header(msg))
+		
