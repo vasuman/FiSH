11efb04700550336c77cd5d2d5c4e8be7ea557c2
Vasuman Ravichandran <vasumanar@gmail.com>
2012-09-21 01:16:15 +0530

    Added netifaces support

--- Initiator.py
+++ Initiator.py
@@ -1,22 +1,25 @@
+#!/usr/bin/env python2
 import os
 import socket as sck
 from LPDoL import *
 import uuid
 import sys
 import socket
+from netifaces import ifaddresses, interfaces, AF_INET
 
 def create_fish_net(uid,mcast):
 	self_obj=Peer(uid,get_ip(),0)
-	peer_list=[self_obj]
-	i=Inducter(uid,mcast,peer_list)
+	i=Inducter(uid,mcast,[self_obj])
 	return i
 
 def get_ip():
-	s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-	s.connect(('google.com',9))
-	ip=s.getsockname()[0]
-	s.close()
-	return ip
+	for iface in interfaces():
+		if_addr=ifaddresses(iface)
+		if AF_INET in if_addr:
+			addr=if_addr[AF_INET][0]['addr']
+			if not addr=='127.0.0.1':
+				return addr
+	return ''
 
 if __name__=='__main__':
 	uid=str(uuid.uuid4())
@@ -25,8 +28,10 @@ if __name__=='__main__':
 		b=Beacon(uid,mcast)
 		b.burn(2)
 		print b.peer_list
+		
 	except PeerDiscoveryError as e:
 		if e.err is 4:
+			print b.mc_sock.convo
 			print "No existing network creating a FISH_NET"
 			i=create_fish_net(uid,mcast)
 			i.start()
