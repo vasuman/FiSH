0d14f7a247afc362f5e91411023fd24f39faf75a
Vasuman Ravichandran <vasumanar@gmail.com>
2012-12-11 15:39:00 +0530

    Updated test script to use new Inducter and MessageHandler

--- test.py
+++ test.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python2
-from LPDoL.TwLPD import *
+from LPDoL.multicast import Inducter
+from LPDoL.handler import MessageHandler
+from LPDoL.common import Peer
 from uuid import uuid1
 import netifaces
 from twisted.internet import reactor
-ip='0.0.0.0'
-ifaces=netifaces.interfaces()
-ifaces.remove('lo')
-for iface in ifaces:
-    addr=netifaces.ifaddresses(iface)
-    if 2 in addr:
-        ip=addr[2][0]['addr']
-p=Peer(uuid1().hex,ip)
-i=Inducter(p,('224.0.0.13',8999))
+
+def get_ip():
+	ip='127.0.0.1'
+	ifaces=netifaces.interfaces()
+	ifaces.remove('lo')
+	for iface in ifaces:
+	    addr=netifaces.ifaddresses(iface)
+	    if 2 in addr:
+	        ip=addr[2][0]['addr']
+	return ip
+
+p=Peer(uuid1().hex,get_ip())
+i=Inducter(('224.0.0.13',8999))
+h=MessageHandler(p, i.broadcast)
+i.addHandler(h.handle)
 reactor.listenMulticast(8999,i)
 reactor.run()
