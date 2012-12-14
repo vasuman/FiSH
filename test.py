4391315cc1ad0d6ec48299425019a4ea860deba9
Vasuman Ravichandran <vasumanar@gmail.com>
2012-12-14 14:05:33 +0530

    Removed netifaces dependancy

--- test.py
+++ test.py
@@ -3,20 +3,20 @@ from LPDoL.multicast import Inducter
 from LPDoL.handler import MessageHandler
 from LPDoL.common import Peer
 from uuid import uuid1
-import netifaces
+# import netifaces
 from twisted.internet import reactor
 
-def get_ip():
-    ip='127.0.0.1'
-    ifaces=netifaces.interfaces()
-    ifaces.remove('lo')
-    for iface in ifaces:
-        addr=netifaces.ifaddresses(iface)
-        if 2 in addr:
-            ip=addr[2][0]['addr']
-    return ip
+# def get_ip():
+#     ip='127.0.0.1'
+#     ifaces=netifaces.interfaces()
+#     ifaces.remove('lo')
+#     for iface in ifaces:
+#         addr=netifaces.ifaddresses(iface)
+#         if 2 in addr:
+#             ip=addr[2][0]['addr']
+#     return ip
 
-p=Peer(uuid1().hex,get_ip())
+p=Peer(uuid1().hex,'127.0.0.1')
 i=Inducter(('224.0.0.13',8999))
 h=MessageHandler(p, i.broadcast)
 i.addHandler(h.handle)
