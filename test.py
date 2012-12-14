63f1a7de66fb14a071a9760cbce9062c4cf693a1
Vasuman Ravichandran <vasumanar@gmail.com>
2012-12-14 18:52:40 +0530

    Changed multicast group. Hope it works!!

--- test.py
+++ test.py
@@ -3,21 +3,10 @@ from LPDoL.multicast import Inducter
 from LPDoL.handler import MessageHandler
 from LPDoL.common import Peer
 from uuid import uuid1
-# import netifaces
 from twisted.internet import reactor
 
-# def get_ip():
-#     ip='127.0.0.1'
-#     ifaces=netifaces.interfaces()
-#     ifaces.remove('lo')
-#     for iface in ifaces:
-#         addr=netifaces.ifaddresses(iface)
-#         if 2 in addr:
-#             ip=addr[2][0]['addr']
-#     return ip
-
 p=Peer(uuid1().hex,'127.0.0.1')
-i=Inducter(('224.0.0.13',8999))
+i=Inducter(('224.0.2.38',8999))
 h=MessageHandler(p, i.broadcast)
 i.addHandler(h.handle)
 reactor.listenMulticast(8999,i)
