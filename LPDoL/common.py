94eeb18cf55b15a380a685a964b8cfcffc07d282
Vasuman Ravichandran <vasumanar@gmail.com>
2012-12-16 19:46:40 +0530

    Moved LMessage to handler.py also added field name in Peer object

--- LPDoL/common.py
+++ LPDoL/common.py
@@ -1,14 +1,10 @@
 from collections import namedtuple
 
-Peer=namedtuple('Peer','uid addr')
+Peer=namedtuple('Peer','uid addr name')
 
-MSG_CODES={
-        1:'LPDOL_HOOK',
-        2:'LPDOL_UNHOOK',
-        3:'LPDOL_LIVE',
-        4:'LPDOL_DEAD'}
 
-KEY_MUL_UID=[1]
+INVALID_CHARS=[';',':','\\','/',' ','\'','\"',',']
+
 
 class LPDoLError(Exception):
     '''Base Error class -- all other exceptions inherit from this'''
@@ -40,61 +36,21 @@ def validate_uid(uid):
         raise MessageException(6,'UID not hexadecimal')
     return uid
 
-class LMessage(object):
-    '''All communication between peers is done by objects of this class'''
-    def __init__(self,key=None,data=None,message_str=None):
-        '''Creates a message object from key and data
-        key -- int : refer MSG_CODES variable
-        data -- list of (16 byte hexadecimal UIDs)
-        If a message_str is passed other values are neglected
-        Creates a message object from string of form 
-            <key>:<data>'''
-        if not message_str is None:
-            key,data=self._parse_message(message_str)
-        #Standard validation of key and data
-        assert type(key) == int, 'Invalid key type'
-        assert type(data) == list, 'Invalid data'
-        self._validate_message(key,data)
-        self.key=key
-        self.data=data
-        #DEBUG STATEMENT!!!!!
-        print repr(self)
-    
-    def __repr__(self):
-        '''Human understandable representation of message'''
-        return MSG_CODES[self.key]+':'+';'.join(self.data)
 
-    def __str__(self):
-        '''Returns a serialized representation of object of form
-            <key>:<data>'''
-        return str(self.key)+':'+';'.join(self.data)
-                
-    def _validate_message(self,key,data):
-        #Checking if <key> is valid index
-        if not key in MSG_CODES.keys():
-            raise MessageException(4,'Invalid key index')
-        #Checking all UIDs
-        map(validate_uid,data)
-        #Checking for underflow
-        if len(data) == 0:
-            raise MessageException(8,'Too few arguments')
-        #Only certain message is supposed to have multiple UIDs
-        if (not key in KEY_MUL_UID) and len(data) != 1:
-                raise MessageException(7,'Too many UIDs')
+def validate_name(name_str):
+    assert type(name_str)==str, 'Name must be a string'
+    #Must not exceed maximum length
+    if len(name_str)>25:
+        raise MessageException(10,'Name is too long')
+    #Must not contain invalid characters
+    for ch in INVALID_CHARS:
+        if ch in name_str:
+            raise MessageException(11,'Name contains invalid characters')
 
-    def _parse_message(self,message):
-        #Checking for class seperator
-        if not ':' in message:
-            raise MessageException(1,'Malformed message string')
-        chop_msg=message.split(':')
-        #Checking if type '<key>:<message>'
-        if not len(chop_msg) == 2:
-            raise MessageException(2,'Invalid message parameters')
-        #Checking if type(<key>) is int
-        try:
-            key=int(chop_msg[0])
-        except:
-            raise MessageException(3,'Invalid key type')
-        data_str=chop_msg[1]
-        data=data_str.split(';')
-        return (key,data)
+def validate_identity(id_tuple):
+    if len(id_tuple) != 2:
+        raise MessageException(9, 'Invalid number of ID parameters')
+    uid,name=id_tuple
+    validate_name(name)
+    validate_uid(uid)
+
--- LPDoL/handler.py
+++ LPDoL/handler.py
@@ -1,69 +1,151 @@
 from common import *
 from twisted.internet import reactor
 
+MSG_CODES={
+        1:'LPDOL_HOOK',
+        2:'LPDOL_UNHOOK',
+        3:'LPDOL_LIVE',
+        4:'LPDOL_NAMETAKEN'}
+
+KEY_MUL_MEM=[1]
+
+VALID_FN={
+    1:validate_identity,
+    2:validate_identity,
+    3:validate_identity,
+    4:validate_identity,
+}
+
+class LMessage(object):
+    '''All communication between peers is done by objects of this class'''
+    def __init__(self,key=None,data=None,message_str=None):
+        '''Creates a message object from key and data
+        key -- int : refer MSG_CODES variable
+        data -- list of data members
+        If a message_str is passed other values are neglected
+        Creates a message object from string of form 
+            <key>:<data>'''
+        if not message_str is None:
+            key,data=self._parse_message(message_str)
+        #Standard validation of key and data
+        assert type(key) == int, 'Invalid key type'
+        assert type(data) == list, 'Invalid data'
+        self._validate_message(key,data)
+        self.key=key
+        self.data=data
+        #DEBUG STATEMENT!!!!!
+        print repr(self)
+    
+    def __repr__(self):
+        '''Human understandable representation of message'''
+        return MSG_CODES[self.key]+':'+';'.join(['.'.join(it) for it in self.data])
+
+    def __str__(self):
+        '''Returns a serialized representation of object of form
+            <key>:<data>'''
+        return str(self.key)+':'+';'.join(['.'.join(it) for it in self.data])
+                
+    def _validate_message(self,key,data):
+        #Checking if <key> is valid index
+        if not key in MSG_CODES.keys():
+            raise MessageException(4,'Invalid key index')
+        validation_function=VALID_FN[key]
+        #Checking all data items
+        map(validation_function,data)
+        #Checking for underflow
+        if len(data) == 0:
+            raise MessageException(8,'Too few arguments')
+        #Only certain message is supposed to have multiple data members
+        if (not key in KEY_MUL_MEM) and len(data) != 1:
+                raise MessageException(7,'Too many members')
+
+    def _parse_message(self,message):
+        #Checking for class seperator
+        if not ':' in message:
+            raise MessageException(1,'Malformed message string')
+        chop_msg=message.split(':')
+        #Checking if type '<key>:<message>'
+        if not len(chop_msg) == 2:
+            raise MessageException(2,'Invalid message parameters')
+        #Checking if type(<key>) is int
+        try:
+            key=int(chop_msg[0])
+        except:
+            raise MessageException(3,'Invalid key type')
+        data_str=chop_msg[1]
+        data=data_str.split(';')
+        data=[tuple(it.split('.')) for it in data]
+        return (key,data)
+
 class MessageHandler(object):
     def __init__(self, host_peer, ofstream):
         self.op_func=ofstream
         self.host=host_peer
         self.peer_list=set([])
+        self.hook_gap=1
         #Mapping message keys to handling functions
         self.FUNC_CODES={
                 1:self._respond_hook,
                 2:self._del_peer,
-                3:self._add_peer}
+                3:self._add_peer,
+                4:self._name_taken}
         #Add HOOK trigger
-        reactor.addSystemEventTrigger('after', 'startup', self.hook, 1)
+        reactor.addSystemEventTrigger('after', 'startup', self.hook)
         #Add UNHOOK trigger
         reactor.addSystemEventTrigger('before', 'shutdown', self.unhook)
 
     def _respond_hook(self,source_peer,message):
+        if source_peer.name in [x.name for x in peer_list]:
+            if source_peer.name != 'anon':
+                self.naming_conflict(source_peer)
+                return             
         self.peer_list.add(source_peer)
         if not self.host.uid in message.data:
             self.live()
 
+    def _name_taken(self,source_peer,message):
+        raise PeerDiscoveryError(1,'Name is already taken')
+            
     def _add_peer(self,source_peer,message):
         self.peer_list.add(source_peer)
     
     def _del_peer(self,source_peer,message):
         self.peer_list.discard(source_peer)
 
-    #Changes need to be made to LMessage befor we can register custom handlers
-    #--validation of UIDs
-    # def registerCustomHandler(self, message_key, handlingFn):
-    #     if not callable(handlingFn):
-    #         return -2
-    #     if message_key in self.FUNC_CODES:
-    #         return -1
-    #     self.FUNC_CODES[message_key]=handlingFn
-
     def handle(self,data,ip):
         try:
             message=LMessage(message_str=data)
         except MessageException as e:
+            print e
             return
-        source_peer=Peer(message.data[0], ip)
+        source_uid, source_name=message.data[0]
+        source_peer=Peer(uid=source_uid, name=source_name, addr=ip)
         self.FUNC_CODES[message.key](source_peer, message)
         
-    def hook(self, this_call):
-        '''Broadcasts a FISH_HOOK on Multicast group'''
+    def hook(self):
+        '''Broadcasts a LPDOL_HOOK on Multicast group'''
         #Generate list of peer UIDs
-        peer_uid=map(lambda x:x.uid, self.peer_list)
+        peer_uid=map(lambda x:(x.uid, x.name), self.peer_list)
         #Prefix host UID to list
-        peer_uid.insert(0,self.host.uid)
+        peer_uid.insert(0,(self.host.uid, self.host.name))
         message=LMessage(1,peer_uid)
         self.op_func(message)
         #Generate next time gap -- Exponential back-off
-        next_call=this_call
-        if this_call < 256: next_call*=2
+        if self.hook_gap < 256: self.hook_gap*=2
         #Register trigger with reactor
-        reactor.callLater(next_call, self.hook, next_call)
-        
+        reactor.callLater(self.hook_gap, self.hook)
+    
+    def naming_conflict(self,source_peer):
+        '''Alerts a peer that the name is already in use'''
+        message=LMessage(4,[(source_peer.uid, source_peer.name)])
+        self.op_func(message)
+
     def live(self):
-        '''Asserts existance by broadcasting FISH_LIVE'''
-        message=LMessage(3,[self.host.uid])
+        '''Asserts existance by broadcasting LPDOL_LIVE'''
+        message=LMessage(3,[(self.host.uid, self.host.name)])
         self.op_func(message)
 
     def unhook(self):
-        '''Broadcasts a FISH_UNHOOK message before disconnecting'''
-        message=LMessage(2,[self.host.uid])
+        '''Broadcasts a LPDOL_UNHOOK message before disconnecting'''
+        message=LMessage(2,[(self.host.uid, self.host.name)])
         self.op_func(message)
