5ef9b657d7914821e83ffd64c3cbf2964afd54a9
Vasuman Ravichandran <vasumanar@gmail.com>
2012-12-16 19:47:32 +0530

    Modified to use new peer

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from LPDoL.common import Peer
 from uuid import uuid1
 from twisted.internet import reactor
 
-p=Peer(uuid1().hex,'127.0.0.1')
+p=Peer(uid=uuid1().hex,name='anon',addr='127.0.0.1')
 i=Inducter(('224.0.2.38',8999))
 h=MessageHandler(p, i.broadcast)
 i.addHandler(h.handle)
