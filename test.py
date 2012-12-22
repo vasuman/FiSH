d3ecd804925e7961c62796ab08ede1506eb3db2a
Vasuman Ravichandran <vasumanar@gmail.com>
2012-12-23 01:27:39 +0530

    Reactor is blocking

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python2
+
 from LPDoL.multicast import Inducter
 from LPDoL.handler import MessageHandler
 from LPDoL.common import Peer
 from uuid import uuid1
 from twisted.internet import reactor
 
-p=Peer(uid=uuid1().hex,name='anon',addr='127.0.0.1')
+p=Peer(uid=uuid1().hex, name='anon',addr='127.0.0.1')
 i=Inducter(('224.0.2.38',8999))
+reactor.listenMulticast(8999,i)
 h=MessageHandler(p, i.broadcast)
 i.addHandler(h.handle)
-reactor.listenMulticast(8999,i)
 reactor.run()
