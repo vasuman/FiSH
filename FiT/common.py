2151624b0f496aa32f520d9d2034184b756a0262
Vasuman Ravichandran <vasumanar@gmail.com>
2012-12-23 01:30:41 +0530

    File transfer also uses LMessage now

--- FiT/common.py
+++ FiT/common.py
@@ -1,6 +1,35 @@
 #common.py
+import util.message as m
 
+def is_error(error_str):
+    if not '.' in error_str:
+        return False
+    try:
+        code, msg=error_str.split('.')
+        code=int(code)
+        assert type(msg)==str
+    except:
+        return False
+    return True
 
+def verify_sha1(sha):
+    #Check for 40 byte string
+    if not len(sha) == 40:
+        return False
+    #Check if valid hexadecimal number
+    try:
+        int(sha,16)
+    except:
+        return False
+    return True
+
+m.MSG_CODES_VALID={
+        1:('LIST_HASH_TABLE',lambda x:x == 1),
+        2:('LOAD_FILE',verify_sha1),
+        3:('START_TRANSFER',lambda x:x == 3),
+        4:('ERROR',is_error)}
+
+m.KEY_MUL_MEM=[]
 
 class FTError(Exception):
     '''Base Error class -- all other exceptions inherit from this'''
@@ -8,57 +37,17 @@ class FTError(Exception):
         self.err=err
         self.message=message
 
+    def __str__(self):
+        return '{0.err}.{0.message}'.format(self)
+
 class IndexerException(FTError):
     '''Is raised when an error occurs while accessing requested file'''
-    def __str__(self):
+    def __repr__(self):
         return 'FILE_EXCEPTION - [ERROR {0.err}]: {0.message}'.format(self)
 
-class IdentException(FTError):
-    '''Is raised when invalid credentials supplied'''
-    def __str__(self):
-        return 'INVALID_IDENTITY - [ERROR {0.err}]: {0.message}'.format(self)
-
-# def validate_uid(uid):
-#     assert type(uid)==str, 'UID must be a string'
-#     #Check for 32 byte string
-#     if not len(uid) == 32:
-#         raise IdentException(1,'Invalid UID length')
-#     #Check if valid hexadecimal number
-#     try:
-#         int(uid,16)
-#     except:
-#         raise IdentException(2,'UID not hexadecimal')
-
-# def validate_name(name_str):
-#     assert type(name_str)==str, 'Name must be a string'
-#     if len(name_str)>25:
-#         raise IdentException(4,'Name is too long')
-#     for ch in INVALID_CHARS:
-#         if ch in name_str:
-#             raise IdentException(3,'Name contains invalid characters')
-#     # contains=reduce((lambda x,y: x or y), [ch in name_str for ch in INVALID_CHARS], False)
-
-
-
-# class IdentString(object):
-#     def __init__(self, name=None, uid=None, data_str=None): 
-#         if not data_str is None:
-#             data=self._parse_message(data_str)
-#             uid, name=data
-#         validate_name(name)
-#         validate_uid(uid)
-#         self.name = name
-#         self.uid = uid
-
-#     def _parse_message(self, data_str):
-#         if not ':' in data_str:
-#             raise IdentException(5,'Delimiter not present')
-#         data=data_str.split(':')
-#         if len(data) != 2:
-#             raise IdentException(6,'Invlaid no. of fields')
-#         return data
-
-#     def __str__(self):
-#         return '{0.uid}:{0.name}'.format(self)
+class DaemonException(FTError):
+    '''Is raised when an invalid FiTd call occurs'''
+    def __repr__(self):
+        return 'FITD_XCP - [ERROR {0.err}]: {0.message}'.format(self)
 
 
